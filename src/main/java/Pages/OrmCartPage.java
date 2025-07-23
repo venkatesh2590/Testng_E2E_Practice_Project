e0a362e17e5417bbd5b5f86aba930c7d6b1c1de1
venkatesh <venkatesh20591@gmail.com>
2025-07-23 13:04:15 +0530

    This is my pull request purpose

---
+++ src/main/java/Pages/OrmCartPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class OrmCartPage {
+	
+	public static void main(String[] args) {
+		System.out.println("this is my cart page 1st pull request purpose");
+		System.out.println("create pull request based on this update");
+	}
+}
