438933bf642bb13a2058e0b6bd6bf304e75dcba5
venkatesh <venkatesh20591@gmail.com>
2025-07-23 13:09:37 +0530

    added page actions

--- src/main/java/Pages/OrmCartPage.java
+++ src/main/java/Pages/OrmCartPage.java
@@ -2,6 +2,10 @@ package Pages;
 
 public class OrmCartPage {
 	
+	public OrmCartPage() {
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("this is my cart page 1st pull request purpose");
 		System.out.println("create pull request based on this update");
